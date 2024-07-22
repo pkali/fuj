0ede3eca8184a8ac17d476054a9c5eddc37a71ac
pkali <pawel.kalinowski@gmail.com>
2024-07-22 17:42:19 -0400

    looks like it is working :O

--- fuj.asm
+++ fuj.asm
@@ -1,6 +1,11 @@
 .macro ver
     .sbyte '0.02'
 .endm  
+DSWRIT = $80
+DSREAD = $40
+DEVIDN = $70
+slot=4
+
 
 .macro gosio
     mva #_dev DDEVIC
@@ -21,70 +26,101 @@
     org $2000
 start
     
-    .local
-    ; close DIR
-    _dev = $70      ; FJ itself
-    _dtimlo = $0f   ; timeout in s
-    _dunit = 1      ; device #
+    lda #<DCB_close_directory
+    ldy #>DCB_close_directory
+    jsr DOSIOV
+            
+    lda #<DCB_mount_host
+    ldy #>DCB_mount_host
+    jsr DOSIOV
+  
+    lda #<DCB_open_directory
+    ldy #>DCB_open_directory
+    jsr DOSIOV
 
-    _dcmnd = $F5    ; close directory
-    _dbyt = 0
-    _dstats = 0
-    _daux1 = 4      ; slot
-    _daux2 = 0
-    gosio
-    .endl
-    
-    .local
-    ; mount host
-    _dev = $70      ; FN itself
-    _dtimlo = $0f   ; timeout in s
-    _dunit = 1      ; device #
-
-    _dcmnd = $F9    ; mount host (slot number)
-    _dbyt = 0
-    _dstats = 0
-    _daux1 = 4      ;slot
-    _daux2 = 0
-    gosio
-    .endl
-    
-    .local
-    ; open DIR
-    _dev = $70      ; FN itself
-    _dtimlo = $0f   ; timeout in s
-    _dunit = 1      ; device #
-    _dbyt = $100    ; buffer length
-
-    _dcmnd = $F7    ; open directory
-    _dstats = $80
-    _dbufa = directory_path
-    _daux1 = 4
-    _daux2 = 1
-    gosio
-    .endl
-
-    .local 
-    ; read DIR
-    _dev = $70      ; FN itself
-    _dtimlo = $0f   ; timeout in s
-    _dunit = 1      ; device #
-    _dbyt = 38+10   ; buffer length (screen width + 10 status bytes)
-
-    _dcmnd = $F6    ; read directory entry
-    _dstats = $40
-    _dbufa = $bc40  ;small_buffer
-    _daux1 = 38+10  ; max dir entry length
-    _daux2 = $80    ; with file status bytes
-    gosio
-    
-	.endl
-
+loop
+    lda #<DCB_read_dir    
+    ldy #>DCB_read_dir
+    jsr DOSIOV
+    lda $bc40
+    cmp #$7f
+    bne loop
 
     ;halt %00000111
     rts
+
+    
+DOSIOV: STA DODCBL+1    ; Set source address
+    STY DODCBL+2
+    LDY #$0B            ; 12 bytes
+DODCBL  LDA $FFFF,Y     ; Changed above.
+    STA DCB,Y           ; To DCB table
+    DEY                 ; Count down
+    BPL DODCBL          ; Until done
+
+SIOVDST:    
+    JSR SIOV            ; Call SIOV
+    LDY DSTATS          ; Get STATUS in Y
+    RTS         
+    
+    
 directory_path
-    .byte '/games/', 0
+    .by "/games/" 0, 0, 0, 0, 0
+
+DCB_open_directory
+    .BYTE   DEVIDN          ; DDEVIC
+    .BYTE   $1              ; DUNIT
+    .BYTE   $f7             ; DCOMND
+    .BYTE   DSWRIT          ; DSTATS
+    .WORD   directory_path  ; DBUFA
+    .BYTE   $0F             ; DTIMLO
+    .BYTE   $00             ; DRESVD reserved
+    .WORD   $100            ; DBYT
+    .BYTE   slot            ; DAUX1
+    .BYTE   1               ; DAUX2
+
+
+    ; close DIR
+
+DCB_close_directory
+    .BYTE   DEVIDN          ; DDEVIC
+    .BYTE   $1              ; DUNIT
+    .BYTE   $f5             ; DCOMND
+    .BYTE   0               ; DSTATS
+    .WORD   0               ; DBUFA
+    .BYTE   $0f             ; DTIMLO
+    .BYTE   0               ; DRESVD reserved
+    .WORD   0               ; DBYT
+    .BYTE   slot            ; DAUX1
+    .BYTE   0               ; DAUX2
+
+DCB_mount_host
+    .BYTE   DEVIDN          ; DDEVIC
+    .BYTE   $1              ; DUNIT
+    .BYTE   $f9             ; DCOMND
+    .BYTE   0               ; DSTATS
+    .WORD   0               ; DBUFA
+    .BYTE   $0f             ; DTIMLO
+    .BYTE   0               ; DRESVD reserved
+    .WORD   0               ; DBYT
+    .BYTE   slot            ; DAUX1
+    .BYTE   0               ; DAUX2
+
+
+DCB_read_dir
+    .BYTE   DEVIDN          ; DDEVIC
+    .BYTE   $1              ; DUNIT
+    .BYTE   $f6             ; DCOMND
+    .BYTE   DSREAD          ; DSTATS
+    .WORD   $bc40           ; DBUFA
+    .BYTE   $0f             ; DTIMLO
+    .BYTE   0               ; DRESVD reserved
+    .WORD   40              ; DBYT
+    .BYTE   41              ; DAUX1
+    .BYTE   $80             ; DAUX2
+
+
+
 small_buffer
     :100 .by 0  ;.ds $100
 big_buffer
